d4058b255e146f4ef58a5876221189751f22972d
Yogesh Kumar Sai <yogesh01041961@gmail.com>
2023-02-06 14:12:31 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const path = require('path')
+const axios = require('axios')
+const app = express()
+const port = 5500
+
+app.get('/', (req, res) => {
+  res.sendFile("index.html",  { root: path.join(__dirname) } )
+})
+
+//this block for live server deployement and some part in index.html
+/* 
+    app.get('/api', async(req, res) => {
+        console.log(req._parsedUrl.query)
+        let url = "https://newsapi.org/v2/everything?" + req._parsedUrl.query
+        let r =  await axios(url)
+        let a = r.data;
+        res.json(a)
+    })
+*/
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
