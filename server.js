81aa5dd7bb5b234baf7c4cddceea038c3e9a8c63
Yogesh Kumar Sai <w3yogesh@gmail.com>
2023-02-08 00:42:14 +0530

    online deploy code

--- server.js
+++ server.js
@@ -4,12 +4,15 @@ const axios = require('axios')
 const app = express()
 const port = 5500
 
+
 app.get('/', (req, res) => {
   res.sendFile("index.html",  { root: path.join(__dirname) } )
 })
 
+
+
 //this block for live server deployement and some part in index.html
-/* 
+
     app.get('/api', async(req, res) => {
         console.log(req._parsedUrl.query)
         let url = "https://newsapi.org/v2/everything?" + req._parsedUrl.query
@@ -17,7 +20,6 @@ app.get('/', (req, res) => {
         let a = r.data;
         res.json(a)
     })
-*/
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
